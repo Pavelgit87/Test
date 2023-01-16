f69eaaae566983e342d8cd35934af0c26785eec1
Pavel <bejenar@list.ru>
2023-01-16 22:50:32 +0300

    Add gitignore

--- Homejob_001/Program.cs
+++ Homejob_001/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+
+{
+    System.Console.WriteLine(number1);
+}
+else if (number1 < number2)
+{
+    System.Console.WriteLine(number2);
+}
+if (number2 < number1)
+{
+    System.Console.WriteLine(number2);
+}
+else if (number2 > number1)
+{
+    System.Console.WriteLine(number1);
+}
